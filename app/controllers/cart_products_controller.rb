c74cf339cc88d77d80c1f2b4667067beee853a06
Ljulienng <ljuliennguyen@gmail.com>
2019-03-04 16:27:19 +0100

    Adding order table

--- app/controllers/cart_products_controller.rb
+++ app/controllers/cart_products_controller.rb
@@ -1,4 +1,6 @@
 class CartProductsController < ApplicationController
+  before_action :authenticate_user!, only[:create, :destroy]
+
   def create
     @user_cart = current_user.cart
     @product_selected = Product.find(params[:product_id].to_i)
