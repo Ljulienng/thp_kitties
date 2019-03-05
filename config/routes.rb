875eb194dc50018ea3e900ed124ea878efcc87a4
Arbousied <kiger.maxence@gmail.com>
2019-03-05 14:23:55 +0100

    r

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users, :controllers => {sessions: 'sessions', registrations: 'registrations'}  
+  devise_for :users, :controllers => {sessions: 'sessions', registrations: 'registrations'}
+
+  resources :cart_products, only: [:create, :destroy]
 
   resources :products do
       resources :carts, only: [:destroy, :create, :update, :index]
