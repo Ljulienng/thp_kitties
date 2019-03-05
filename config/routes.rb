ba3d8350bb5ca75c693675660820e31caf1d1829
Ljulienng <ljuliennguyen@gmail.com>
2019-03-05 14:29:48 +0100

    d

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,9 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
-  devise_for :users
-=======
-  devise_for :users, :controllers => {sessions: 'sessions', registrations: 'registrations'}  
->>>>>>> 62ad1b795ee410af8ef4a892074755668bb0d807
+  devise_for :users, :controllers => {sessions: 'sessions', registrations: 'registrations'}
+
 
   resources :products do
-    resources :products_avatars, only: [:create]  
+    resources :products_avatars, only: [:create]
     resources :carts, only: [:destroy, :create, :update, :index]
   end
   resources :users do
