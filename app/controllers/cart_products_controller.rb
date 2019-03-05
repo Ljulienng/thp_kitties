f1676ff561642300fdd86f7390adea831ef1088a
Ljulienng <ljuliennguyen@gmail.com>
2019-03-06 00:26:34 +0100

    who the fck puts an only without the ":"

--- app/controllers/cart_products_controller.rb
+++ app/controllers/cart_products_controller.rb
@@ -1,5 +1,5 @@
 class CartProductsController < ApplicationController
-  before_action :authenticate_user!, only[:create, :destroy]
+  before_action :authenticate_user!, only: [:create, :destroy]
 
   def create
     @user_cart = current_user.cart
