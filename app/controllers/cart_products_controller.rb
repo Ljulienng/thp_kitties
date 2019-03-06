5e8722c310335756a635867661a5aeb3b5e28eac
thomasfrl <feral.thomas@gmail.com>
2019-03-06 11:35:50 +0100

    merge

--- app/controllers/cart_products_controller.rb
+++ app/controllers/cart_products_controller.rb
@@ -1,9 +1,5 @@
 class CartProductsController < ApplicationController
-<<<<<<< HEAD
   before_action :authenticate_user!, only: [:create, :destroy]
-=======
-  before_action :authenticate_user!
->>>>>>> 1572a90929ad4d0d3d7cd28ee1d5f1cf906b0473
 
   def create
     @user_cart = current_user.cart
