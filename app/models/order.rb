5d424a9e697f4c22bf584dbe0f815ea7a4dbf3f2
Florentbilocq <florentbilocq@gmail.com>
2019-03-07 11:10:13 +0100

    ordermodel

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,15 +1,10 @@
 class Order < ApplicationRecord
-<<<<<<< HEAD
-  has_one :user
-  has_many :cart_products
+  belongs_to :user
+  belongs_to :cart
   after_create :confirm_order
 
   def confirm_order
     UserMailer.order_email(self).deliver_now
   end
 
-=======
-  belongs_to :user
-  belongs_to :cart
->>>>>>> 7c218f6a4eec6c33d1e1388325d3f74f01f16b2c
 end
